0aff1f2260d3e69ac07286910c0cebcd62be6096
Dasha Nos <dasha.nos91@gmail.com>
2019-10-16 20:27:53 +0300

    test amount = 0

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    void shouldReturn1000IfAmountIs0() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
     }
 }
