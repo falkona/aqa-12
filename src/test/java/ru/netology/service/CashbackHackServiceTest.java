e463e32c96a96250e0cc1c7b195e5af5055a787b
Dasha Nos <dasha.nos91@gmail.com>
2019-10-16 20:29:47 +0300

    tests amount = 1000, 400, 1900

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -14,4 +14,31 @@ class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn600IfAmountIs400() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(400);
+        int expected = 600;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn100IfAmountIs1900() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1900);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
 }
